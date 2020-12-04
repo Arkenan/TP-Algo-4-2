e60db2ce5e03fc8c84d5ae3e2fa6dafd28ecb3f0
Lucia Kasman <lkasman@medallia.com>
2020-12-04 01:06:52 -0300

    add random forest regressor in spark pipeline

--- src/main/scala/fiuba/fp/SparkRegressor.scala
+++ src/main/scala/fiuba/fp/SparkRegressor.scala
@@ -1,25 +1,55 @@
 package fiuba.fp
 
 import fiuba.fp.models.DataFrameRow
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.regression.RandomForestRegressor
+import org.apache.spark.ml.{Pipeline, Transformer}
 import org.apache.spark.sql.SparkSession
 
 /**
  * Collection of Machine Learning methods to train and test a model given a training and testing sets of DataFrameRows
  */
 object SparkRegressor {
-  def trainAndTest(sets : (List[DataFrameRow], List[DataFrameRow])) : Unit = trainAndTest(sets._1, sets._2)
+  def trainAndTest(sets: (List[DataFrameRow], List[DataFrameRow])): Unit = trainAndTest(sets._1, sets._2)
 
   def trainAndTest(trainingSet: List[DataFrameRow], testingSet: List[DataFrameRow]): Unit = {
+    randomForestRegression(trainingSet, testingSet, "close")
+  }
+
+  def randomForestRegression(trainingSet: List[DataFrameRow], testingSet: List[DataFrameRow], target: String): Unit = {
+
     val spark = SparkSession.builder()
       .master("local[*]")
       .getOrCreate()
 
     import spark.implicits._
 
-    val df = trainingSet.toDF()
+    val trainingSetDs = trainingSet.toDS()
 
-    print(f"Training Set length=${trainingSet.length}\n")
-    print(f"Testing Set length=${testingSet.length}\n")
-    df.show(5)
+    val assembler = new VectorAssembler()
+      .setInputCols(trainingSetDs.columns)
+      .setOutputCol("features")
+
+    val indexer = new StringIndexer()
+      .setInputCol(target)
+      .setOutputCol("label")
+
+    val randomForestRegressor = new RandomForestRegressor()
+      .setMaxDepth(3)
+      .setNumTrees(20)
+      .setFeatureSubsetStrategy("auto")
+      .setSeed(2504)
+      .setFeaturesCol("features")
+      .setLabelCol(target)
+      .setPredictionCol("prediction")
+
+    val stages = Array(assembler, indexer, randomForestRegressor)
+
+    val sparkTransformer: Transformer = new Pipeline().setStages(stages)
+      .fit(trainingSetDs)
+
+    val result = sparkTransformer.transform(testingSet.toDS())
+
+    print(result)
   }
 }
