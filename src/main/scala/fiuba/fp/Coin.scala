603eb45669636620a0d4727df8c6218794af1ecc
Tomás Arjovsky <t.arjovsky@gmail.com>
2021-02-25 20:53:29 -0300

    Modularize Splitter and Coin to be more testable

--- src/main/scala/fiuba/fp/Coin.scala
+++ src/main/scala/fiuba/fp/Coin.scala
@@ -2,13 +2,18 @@ package fiuba.fp
 
 import scala.util.Random
 
+/** A coin that can be flipped, returning a new coin and the result (true=heads, false=tails). */
+trait Coin {
+  def flip(): (Coin, Boolean)
+}
+
 /** Represents a Bernoulli trial with p=chance.
  *
  * In detail: it throws true or false with the probability stated in chance. It produces a new coin to throw and get
  * a different result. The same coin will always produce the same result, so to produce a new one, either create a new
  * coin with a different seed or use the one provided by the flip method.
  */
-class WeightedCoin(chance: Float, seed: Long) {
+class WeightedCoin(chance: Float, seed: Long) extends Coin {
   /** Produces a new coin and a True/False result. */
   def flip(): (WeightedCoin, Boolean) =  {
     val rnd = new Random(seed)
--- src/main/scala/fiuba/fp/Splitter.scala
+++ src/main/scala/fiuba/fp/Splitter.scala
@@ -3,14 +3,14 @@ package fiuba.fp;
 import scala.collection.immutable.List;
 
 /** Uses a WeightedCoin to randomly add elements to one list or the other. */
-class Splitter[A](wc: WeightedCoin, l1:List[A], l2: List[A]) {
+class Splitter[A](coin: Coin, l1:List[A], l2: List[A]) {
   /** Produces a new Splitter with the element added in one list or the other randomly. */
   def feed(element: A) : Splitter[A] = {
-    val (wc2, l) = wc.flip()
+    val (coin2, l) = coin.flip()
     if (l)
-      new Splitter(wc2, element::l1, l2)
+      new Splitter(coin2, element::l1, l2)
     else
-      new Splitter(wc2, l1, element::l2)
+      new Splitter(coin2, l1, element::l2)
   }
 
   /** Gets both lists in a two-element tuple. */
